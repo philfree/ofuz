ba98b7867161294b03b0984ca7fe25edcf7d9ab1
Philippe Lewicki <philippe@ofuz.org>
2011-08-27 12:07:30 -0700

    add 2011 in copyright, moved html tag at bottom

--- project.php
+++ project.php
@@ -1,5 +1,5 @@
 <?php 
-// Copyrights 2008 - 2010 all rights reserved, SQLFusion LLC, info@sqlfusion.com
+// Copyrights 2008 - 2011 all rights reserved, SQLFusion LLC, info@sqlfusion.com
 /** Ofuz Open Source version is released under the GNU Affero General Public License, please read the full license at: http://www.gnu.org/licenses/agpl-3.0.html **/
 
     $pageTitle = 'Ofuz :: Project';
