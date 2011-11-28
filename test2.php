709ab5b1d5ec9945ba53c7c1a2b6aa238c199b0e
sqlfusion2 <sqlfusion2@sqlfusion2-desktop.(none)>
2011-11-28 21:28:06 +0530

    test file for drop box mail receiver

---
+++ test2.php
@@ -0,0 +1,67 @@
+<?php
+include_once('config.php');
+
+$savefile = "savehere.txt";
+
+$sf = fopen($savefile, 'a+') or die("can't open file");
+ob_start();
+
+// read from stdin
+$fd = fopen("php://stdin", "r");
+$email = "";
+while (!feof($fd)) {
+	$email .= fread($fd, 1024);
+}
+fclose($fd);
+// handle email
+
+//$lines = explode("\n", $email);
+
+$OfuzEmailFetcher = new OfuzEmailFetcher();
+$OfuzEmailFetcher->fetchEmailRow($email);
+
+$to = $OfuzEmailFetcher->getToEmail();
+$from = $OfuzEmailFetcher->getFromEmail();
+$bcc = $OfuzEmailFetcher->getBCCEmail();
+$cc = $OfuzEmailFetcher->getCCEmail(); 
+
+//$email_marged_array = array_merge($to,$bcc);
+$all_emails_merged = array_merge($to,$cc);
+//print_r($all_emails_merged);
+
+if(is_array($all_emails_merged)){
+  foreach($all_emails_merged as $email_add){
+	  
+    $display_array[] = $OfuzEmailFetcher->getEmailAddress($email_add);
+    $email_array[] = $OfuzEmailFetcher->getEmailDisplay($email_add);
+  }
+  $len_to_emailarr = count($email_array);
+}
+
+
+$drop_box_list = array("addnote-","addtask-","task-","newtask-");
+
+for($i=0 ;$i<$len_to_emailarr;$i++){
+	
+    $email_code_split = split("@", $email_array[$i]);
+    $email_code = $email_code_split[0];
+    $email_code_domain = $email_code_split[1];
+    if($email_code_domain == $GLOBALS['email_domain']){
+	foreach($drop_box_list as $key=>$value){	 
+		if(preg_match("/$value/",$email_code,$matches)) { 
+			$valid = 1;
+			break;
+        }
+	  }
+    }
+}
+
+
+if($valid){
+//echo $email;die();	
+fwrite($sf,"$message");
+ob_end_clean();
+}
+
+fclose($sf);
+?>
