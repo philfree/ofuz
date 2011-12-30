4917b4c35eaa708823b79c23ec3babc7e3e088ee
Philippe Lewicki <philippe@ofuz.org>
2011-12-30 08:00:47 -0800

    moved plugin sample classes to a class folder

---
+++ plugin/SamplePlugIn/class/SamplePlugIn.class.php
@@ -0,0 +1,27 @@
+<?php
+// Copyright 2008-2012 SQLFusion LLC           info@sqlfusion.com
+/**COPYRIGHTS**/
+
+  /**
+   * SamplePlugIn Object
+   * Used to illustrate the EventAction hooks
+   *
+   * @package SamplePlugIn
+   * @author Philippe Lewicki <phil@sqlfusion.com>
+   * @license ##License##
+   * @version 0.1
+   * @date 2011-12-29  
+   */
+
+
+class SamplePlugIn extends BaseObject {
+	
+	function eventSampleAction(EventControler $evctl) {
+		
+		
+	}
+	
+}
+
+
+?>
