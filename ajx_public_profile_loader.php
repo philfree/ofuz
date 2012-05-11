b580d669b78e2da0cb2f92cbe8855c347757b6d3
Ravi Rokkam <ravi@phil-MacBookPro.(none)>
2012-05-11 23:15:35 +0530

    updated public/mobile profile

--- ajx_public_profile_loader.php
+++ ajx_public_profile_loader.php
@@ -99,7 +99,7 @@ if ($ContactPhone->getNumRows()) {
 $profile_html .= '</div>';
 if(!empty($contact_no)) {
 $profile_html .= '<div class="layout_add">';
-$profile_html .= '<form method="post" action="'.$_SERVER['PHP_SELF'].'">';
+$profile_html .= '<form method="post" action="/public_profile.php">';
 $profile_html .= '<input class="profile_button" type="image" src="/images/profile_add_to_ofuz.png" alt="Add the contact to Ofuz" name="add_cont" />';
 $profile_html .= '<a href="/public_profile_vcard.php"><img src="/images/profile_add_to_phone.png" width="91" height="26" alt="" /></a>';
 $profile_html .= '<input type="hidden" name="hd_add_cont" value="1" />';
@@ -126,18 +126,21 @@ $profile_html .= '</div></div>';
 $profile_html .= '<div class="profile_web_layout_main">';
 $profile_html .= '<div class="profile_web_layout_inner">';
 $profile_html .= '<div class="profile_web_layout_photo_block">';
+
 $profile_html .= '<div class="profile_web_layout_photo_left">';
+$profile_html .= '<div class="profile_photo">';
 if ($_SESSION['do_contact']->picture != '') {
-$profile_html .= '<img src="'.$_SESSION['do_contact']->getContactPicture().'" height="100%"  width = 100% alt="" />';
+$profile_html .= '<img src="'.$_SESSION['do_contact']->getContactPicture().'" height="120px"  width="120px" alt="" />';
 } else {
 $profile_html .= '<img src="/images/empty_avatar.gif" height="100%"  width = 100% alt="" />';
 }
+$profile_html .= '</div>';
 $profile_html .= '</div>';
 
 $profile_html .= '<div class="profile_web_layout_photo_right">';
 $profile_html .= '<div class="profile_name">';
 $profile_html .= '<div style="position:relative;float:left;width:70%;">'.$_SESSION['do_contact']->firstname.' '.$_SESSION['do_contact']->lastname.'</div>';
-$profile_html .= '<div style="position:relative;float:right;width:15%;"><a href="/public_profile_vcard.php"><img class="profile_button" src="/images/profile_add_to_phone.png" width="91" height="26" alt="" /></div>';
+$profile_html .= '<div style="position:relative;float:right;width:15%;font-size:12px;"><a href="/public_profile_vcard.php">download VCard</div>';
 $profile_html .= '<div style="position:relative;float:right;width:15%;">';
 $profile_html .= '<form method="post" action="/public_profile.php">';
 $profile_html .= '<input class="profile_button" type="image" src="/images/profile_add_to_ofuz.png" alt="Add the contact to Ofuz" name="add_cont" />';
