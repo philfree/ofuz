c2c65f11161c7322ccf4f324832b5823f342a8bb
vivekshekar <vivek@sqlfusion.com>
2013-11-23 00:01:47 +0530

    Added db changes

--- update_db.sql
+++ update_db.sql
@@ -114,3 +114,16 @@ CREATE TABLE IF NOT EXISTS `cc_details` (
   PRIMARY KEY (`idcc_details`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
+CREATE TABLE `ofuzdev`.`user_gitrepo` (
+`iduser_gitrepo` INT( 50 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`iduser` INT( 50 ) NOT NULL ,
+`git_repo` VARCHAR( 100 ) NOT NULL ,
+`git_repourl` VARCHAR( 200 ) NOT NULL
+) ENGINE = MYISAM
+
+CREATE TABLE `ofuzdev`.`git_project` (
+`idgit_project` INT( 12 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`iduser_gitrepo` INT( 12 ) NOT NULL ,
+`idproject` INT( 12 ) NOT NULL
+) ENGINE = MYISAM ;
+ 
