e19eeaf9e5d76ea11d2e2533c9a3557dc2f01d0d
Sarvesh Kulkarni <sarvesh@htmlfusion.com>
2014-06-27 11:28:44 -0700

    reverting to old change

--- class/ProjectDiscuss.class.php
+++ class/ProjectDiscuss.class.php
@@ -297,8 +297,8 @@ class ProjectDiscuss extends Note {
 
 
           try {
-              $co_workers_obj = new Project();
-              $co_workers = $co_workers_obj->getProjectCoWorkers();
+              $co_workers = $_SESSION["do_project"]->getProjectCoWorkers();
+              
                
               if ($co_workers !== false) {
                   $email_template = new EmailTemplate("ofuz_project_discussion");
