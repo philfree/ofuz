e88b7351c79ee8091fc9f98c1d9ec3b56e6db032
Cholleti Varaprasad <varaprasad@htmlfusion.com>
2016-04-20 12:21:02 -0700

    timesheet-changes-16751 and timesheet calendar plugin integration

--- plugin/Timesheets/TabContentTimesheets.php
+++ plugin/Timesheets/TabContentTimesheets.php
@@ -21,3 +21,78 @@ Here is an other example of plug in page: <a href="/PlugIn/Timesheets/Timesheets
 <br/>
 <div class="dottedline"></div>
 The documentation to create Ofuz Plug-in can be found at: <br/> <a href="http://www.ofuz.com/opensource/wiki/plugin_api" title="plugin api documentation">http://www.ofuz.com/opensource/wiki/plugin_api</a>-->
+
+<link href='../../includes/fullcalendar.css' rel='stylesheet' />
+<link href='../../includes/fullcalendar.print.css' rel='stylesheet' media='print' />
+<script src='../../includes/moment.min.js'></script>
+<script src='../../includes/jquery.min.js'></script>
+<script src='../../includes/fullcalendar.min.js'></script>
+<script>
+
+	$(document).ready(function() {
+	
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2016-01-12',
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: {
+				url: 'php/get-events.php',
+				error: function() {
+					$('#script-warning').show();
+				}
+			},
+			loading: function(bool) {
+				$('#loading').toggle(bool);
+			}
+		});
+		
+	});
+
+</script>
+<style>
+
+	body {
+		margin: 0;
+		padding: 0;
+		font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+		font-size: 14px;
+	}
+
+	#script-warning {
+		display: none;
+		background: #eee;
+		border-bottom: 1px solid #ddd;
+		padding: 0 10px;
+		line-height: 40px;
+		text-align: center;
+		font-weight: bold;
+		font-size: 12px;
+		color: red;
+	}
+
+	#loading {
+		display: none;
+		position: absolute;
+		top: 10px;
+		right: 10px;
+	}
+
+	#calendar {
+		max-width: 900px;
+		margin: 40px auto;
+		padding: 0 10px;
+	}
+
+</style>
+	<div id='script-warning'>
+		<!--<code>php/get-events.php</code> must be running.-->
+	</div>
+
+	<div id='loading'>loading...</div>
+
+	<div id='calendar'></div>
