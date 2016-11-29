f26da6a1eaeb4fe460d76973bb94a9e10c182528
Ravi <ravi@htmlfusion.com>
2016-11-29 23:42:42 +0530

    fixed task priority drag-drop

--- project.php
+++ project.php
@@ -59,9 +59,9 @@
         $do_project->setBreadcrumb();
     }
 ?>
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<link href="/jquery/jquery-ui-1.8.24.css" rel="stylesheet" type="text/css"/>
+<script src="/jquery/jquery-1.5.2.min.js"></script>
+<script src="/jquery/jquery-ui-1.8.24.min.js"></script>
   
 <script type="text/javascript">
 /*domStyle.set(dialogObj.containerNode, {
